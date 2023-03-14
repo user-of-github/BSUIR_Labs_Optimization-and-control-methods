30c569e8532d1274a302f2bc33a22390cea6db2a
Nikita <nikitosminsk100@gmail.com>
2023-03-14 19:21:07 +0300

    feat: add LR3

--- LR3/main.py
+++ LR3/main.py
@@ -1,11 +1,13 @@
-import pprint
-from data import MATRIX_A, VECTOR_B, VECTOR_C
+from data import MATRIX_A, VECTOR_B
 from start_phase_of_simplex_method import start_phase_of_simplex_method
 
 
 def main() -> None:
-    response = start_phase_of_simplex_method(MATRIX_A, VECTOR_B, VECTOR_C)
-
+    response = start_phase_of_simplex_method(MATRIX_A, VECTOR_B)
+    print('X = ( ', response[0], ' )')
+    print('B = { ', response[1], ' }')
+    print('A = ( ', response[2], ' )')
+    print('b = ( ', response[3], ' )')
 
 
 if __name__ == '__main__':
--- LR3/start_phase_of_simplex_method.py
+++ LR3/start_phase_of_simplex_method.py
@@ -1,26 +1,22 @@
 import copy
-import pprint
 import numpy as np
+from basic_phase_of_simplex_method import basic_phase_of_simplex_method
 from basic_phase_of_simplex_method import extract_submatrix_by_column_numbers
 
 
-def start_phase_of_simplex_method(matrix_a: np.array, vector_b: np.array, vector_c: np.array) -> (np.array, np.array):
+def start_phase_of_simplex_method(matrix_a: np.array, vector_b: np.array) -> (np.array, np.array, np.array, np.array):
     a: np.array = copy.deepcopy(matrix_a)
     b: np.array = copy.deepcopy(vector_b)
-    c: np.array = copy.deepcopy(vector_c)
 
     rows_count: int = matrix_a.shape[0]
     columns_count: int = matrix_a.shape[1]
 
     # 1) remove negative rows (where b[i] < 0)
-
     for row_index in range(rows_count):
         if b[row_index] < 0:
             for col_index in range(columns_count):
                 a[row_index][col_index] *= -1
             b[row_index] *= -1
-
-    pprint.pprint(a)
 
     # 2) get auxiliary task of linear programming c_auxiliary
     c_auxiliary: np.array = np.array([0] * columns_count + [-1] * rows_count)
@@ -28,11 +24,45 @@ def start_phase_of_simplex_method(matrix_a: np.array, vector_b: np.array, vector
 
     a_auxiliary: np.array = np.append(a, identity_matrix, axis=1)  # glue matrices horizontally
 
+    # 3)
     x_auxiliary_initial_plan: np.array = np.append(np.array([0] * columns_count), b)
-    b_auxiliary_initial_plan: np.array = np.array([columns_count + counter + 1 for counter in range(columns_count)])
+    b_auxiliary_initial_plan: np.array = np.array([columns_count + counter + 1 for counter in range(rows_count)])
 
-    #a_auxiliary_base_matrix: np.array = extract_submatrix_by_column_numbers(a_auxiliary, b_auxiliary_initial_plan)
-    #a_auxiliary_inverse_matrix: np.array = np.linalg.inv(a_auxiliary_base_matrix)
+    # 4) solve auxiliary task by main(basic) phase of simplex method
+    auxiliary_response = basic_phase_of_simplex_method(a_auxiliary, c_auxiliary, x_auxiliary_initial_plan, b_auxiliary_initial_plan)
+    x_basis_plan, b_basis_indexes = auxiliary_response
 
-    print(b_auxiliary_initial_plan)
-    pprint.pprint(a_auxiliary)
+    # 5) check the conditions of jointness // условия совместности
+    for index in range(columns_count, a_auxiliary.shape[0]):
+        if x_basis_plan[index] != 0:
+            raise Exception('The task is not joint and the method completes its work')
+
+    while max(b_basis_indexes) > columns_count + 1:
+        # 6)
+        j_k: int = np.max(b_basis_indexes)
+        k: int = b_basis_indexes.tolist().index(j_k) + 1
+        i: int = j_k - columns_count
+
+        j_vector: list = [i for i in range(1, columns_count + 1) if i <= columns_count + 1 and i not in b_basis_indexes.tolist()]
+
+        a_base_matrix: np.array = extract_submatrix_by_column_numbers(a_auxiliary, b_basis_indexes.tolist())
+        inverse_matrix: np.array = np.linalg.inv(a_base_matrix)
+
+        for j in j_vector:
+            # print('Inverse \n', inverse_matrix)
+            # print('submatrix \n', extract_submatrix_by_column_numbers(a_auxiliary, [j]))
+            l_j = np.matmul(inverse_matrix, extract_submatrix_by_column_numbers(a_auxiliary, [j]))
+            # print(l_j)
+
+            if l_j[k - 1] != 0:
+                b_basis_indexes[k - 1] = j - 1
+            else:
+                a_auxiliary = np.array(a_auxiliary[:j-1])
+                a = np.array(a[:j-1])
+
+                b_basis_indexes = np.delete(b_basis_indexes, np.argwhere(b_basis_indexes == j_k))  # remove by value
+                b = np.delete(b, np.argwhere(b == b[j-1]))
+
+                break
+
+    return x_basis_plan[:columns_count], b_basis_indexes, a, b
