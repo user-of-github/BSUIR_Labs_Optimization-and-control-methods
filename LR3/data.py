a895f661289b8e0aa9c1bbec2d8b0d41325edcb3
Nikita <nikitosminsk100@gmail.com>
2023-04-25 14:14:09 +0300

    Init: start LR5; docs: add LR5 statements

--- LR3/data.py
+++ LR3/data.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-MATRIX_A: np.array = np.array([[1, 1, 1],
-                               [2, 2, 2]])
+MATRIX_A: np.array = np.array(
+    [[1, 1, 1],
+     [2, 2, 2]]
+)
 
 VECTOR_C: np.array = np.array([1, 0, 0])
 VECTOR_B: np.array = np.array([0, 0])
--- LR4/main.py
+++ LR4/main.py
@@ -4,7 +4,7 @@ from data import C_VECTOR, B_VECTOR, B_INDICES_BASE, A_MATRIX
 
 def main() -> None:
     solution = dual_simplex_method(A_MATRIX, B_VECTOR, B_INDICES_BASE, C_VECTOR)
-    print(solution)
+    print('Dual simplex method: ', solution)
 
 
 if __name__ == '__main__':
